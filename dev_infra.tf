4c60288a5c928dfb00342e5d15a0bd990f8db040
alelova <alelova@gmail.com>
2023-02-27 12:47:51 +0100

    update

---
+++ dev_infra.tf
@@ -0,0 +1,64 @@
+variable "whitelist" {
+  type = list(string)
+}
+variable "miaws_key" {
+  type    = string
+  default = "amazon_linux_key_1"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "eu-central-1"
+}
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_subnet" "Public102" {
+  vpc_id     = aws_default_vpc.default.id
+  cidr_block = "172.31.102.0/24"
+
+  tags = {
+    Name = "redhat9_102"
+    "Terraform" = "true"
+  }
+}
+
+resource "aws_security_group" "redhat9_sg"{
+  name = "redhat9_sg"
+  description = " allows standard http y https ports inbound"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.whitelist
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = -1
+    to_port   = -1
+    protocol  = "icmp"
+    cidr_blocks = ["172.31.0.0/16"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    ="-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+tags = { 
+  "Terraform" = "true"
+  }
+}
+
---
+++ dev_server.tf
@@ -0,0 +1,28 @@
+variable "default_instance_type" {
+  type    = string
+  default = "t2.nano"
+}
+variable "default_web_subnet"{
+  type = string
+  default = "aws_subnet.redhat9_102.id"
+}
+data "template_file" "user_data" {
+  template = file("./scripts/default_server.yaml")
+}
+
+
+resource "aws_instance" "rh9-0001" {
+  ami                            = "ami-068638f6d6b1f"
+  instance_type                  = var.default_instance_type
+  key_name                       = var.miaws_key
+  vpc_security_group_ids         = [aws_security_group.redhat9_sg.id]
+  subnet_id                      = aws_subnet.redhat9_102.id
+  associate_public_ip_address    = "true"
+  user_data                      = data.template_file.user_data.rendered
+
+  tags = { 
+    Name = "redhat9-0001"
+    "Terraform" = "true"
+  }
+}
+
