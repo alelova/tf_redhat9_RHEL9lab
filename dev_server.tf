96a7f506064f0b8adf106bd588086a16302f9d6b
alelova <alelova@gmail.com>
2023-02-27 12:55:06 +0100

    anadido servidor

--- dev_server.tf
+++ dev_server.tf
@@ -11,7 +11,7 @@ data "template_file" "user_data" {
 }
 
 
-resource "aws_instance" "rh9-0001" {
+resource "aws_instance" "lab-rh9-0001" {
   ami                            = "ami-068638f6d6b1f"
   instance_type                  = var.default_instance_type
   key_name                       = var.miaws_key
@@ -26,3 +26,17 @@ resource "aws_instance" "rh9-0001" {
   }
 }
 
+resource "aws_instance" "lab-rh9-0002" {
+  ami                            = "ami-068638f6d6b1f"
+  instance_type                  = var.default_instance_type
+  key_name                       = var.miaws_key
+  vpc_security_group_ids         = [aws_security_group.redhat9_sg.id]
+  subnet_id                      = aws_subnet.redhat9_102.id
+  associate_public_ip_address    = "true"
+  user_data                      = data.template_file.user_data.rendered
+
+  tags = { 
+    Name = "redhat9-0001"
+    "Terraform" = "true"
+  }
+}
