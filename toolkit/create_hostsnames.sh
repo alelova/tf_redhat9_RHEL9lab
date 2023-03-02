b23cf6f444c9e1d9833b2178f5c766126664f4c2
alelova <alelova@gmail.com>
2023-03-02 12:28:27 +0100

    set hostnames in servers

---
+++ toolkit/create_hostsnames.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+export servera="$(./aws_describe-instances.sh|grep 001|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}') servera"
+export serverb="$(./aws_describe-instances.sh|grep 002|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}') serverb"
+sudo sed  -i '/server/d' /etc/hosts
+echo $servera | sudo tee --append /etc/hosts
+echo $serverb | sudo tee --append /etc/hosts
+
+#### set servera
+   ssh student@servera sudo hostnamectl set-hostname servera
+   ssh student@servera sudo sed  -i '/server/d' /etc/hosts
+   ssh student@servera "echo "$servera" | sudo tee --append /etc/hosts"
+   ssh student@servera "echo "$serverb" | sudo tee --append /etc/hosts"
+
+#### set serverb
+   ssh student@serverb sudo hostnamectl set-hostname serverb
+   ssh student@serverb sudo sed  -i '/server/d' /etc/hosts
+   ssh student@serverb "echo "$servera" | sudo tee --append /etc/hosts"
+   ssh student@serverb "echo "$serverb" | sudo tee --append /etc/hosts"
